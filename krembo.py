40a20e2ed2e76503fed0b791d09a896f121d7d06
YoavNir1999 <93100426+YoavNir1999@users.noreply.github.com>
2021-11-03 11:43:37 +0200

    Update krembo.py

--- krembo.py
+++ krembo.py
@@ -7,32 +7,40 @@ def convert_df(df):
 tab = st.container()
 upl = st.container()
 with upl:
-    table = st.file_uploader(label='העלה דוח פעילים',type=['csv'])
+    table = st.file_uploader(label='העלאת דוח פעילים',type=['csv'])
 
 with tab:
     try:
-        st.title('?מה קורה בנות')
+        st.title('!עמדת קליטה')
         st.title('')
         df = pd.read_csv(table)
         df = df.filter(['שם משפחה', 'שם פרטי','תאריך אישור הורים ותקנון','שנת פעילות 2021-22'])
         df = df.iloc[:, ::-1]
-        show1 = st.checkbox('הצג טבלה מקורית')
+        show1 = st.checkbox('הצגת טבלה מקורית')
         if show1:
             st.dataframe(df)
         
         df2 = df
-        filt = st.selectbox('בחר מסנן',['None','הרשמה שנתית תקפה','אישור הורים תקף'])
+        filt = st.selectbox('בחירת מסנן',['None','הרשמה שנתית תקפה','אישור הורים תקף'],index=2)
         if filt == 'הרשמה שנתית תקפה':
             df2 = df2.loc[df['שנת פעילות 2021-22'] == 1.0000]
+            activate_search=st.checkbox('לחצי לחיפוש')
+            if activate_search:
+                search = st.text_input('הכניסי שם פרטי')
+                df2=df2[df2['שם פרטי'].str.contains(search)]
             st.dataframe(df2)
         elif filt == 'אישור הורים תקף':
             df2=df2[df2['תאריך אישור הורים ותקנון'].str.contains('2021', na = False)]
             df2=df2[df2['תאריך אישור הורים ותקנון'].str[3:5:].isin(['09','10','11','12'])]
+            activate_search=st.checkbox('לחצי לחיפוש')
+            if activate_search:
+                search = st.text_input('הכניסי שם פרטי')
+                df2=df2[df2['שם פרטי'].str.contains(search)]
             st.dataframe(df2)
         save = st.checkbox('save?')
         if save:
             csv = convert_df(df2)
-        st.download_button(label="יאללה יאחתי תורידי",data=csv,file_name='krembo list.csv',mime='text/csv')
+        st.download_button(label="הורדת קובץ",data=csv,file_name='krembo list.csv',mime='text/csv')
 
     except:
         pass
